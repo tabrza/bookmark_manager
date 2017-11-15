722fc3033f87958eea03b944ab89eb612d9b1b65
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-11-15 14:19:26 +0000

    adds link and tag models

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,4 @@
 require 'data_mapper'
-require 'dm-postgres-adapter'
 
 class Link
 
@@ -8,8 +7,5 @@ class Link
   property :id, Serial
   property :title, String
   property :url, String
+  has n, :tags, through: Resource
 end
-
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
-DataMapper.finalize
-DataMapper.auto_upgrade!
---
+++ app/models/tag.rb
@@ -0,0 +1,8 @@
+require 'data_mapper'
+
+class Tag
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :name, String
+end
