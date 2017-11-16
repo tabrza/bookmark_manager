f5d1cc91782ba6fc401ca6beb514a562cbee2640
Tabish Raza <tabishraza@gmail.com>
2017-11-16 13:16:45 +0000

    bcrypt method to user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
+require 'bcrypt'
+
 class User
   include DataMapper::Resource
 
   property :id, Serial
   property :email, Text
-  property :password, Text
+  property :encrypted_password, Text
+
+  def password(new_password)
+    self.encrypted_password = BCrypt::Password.create(new_password)
+  end
 
 end
