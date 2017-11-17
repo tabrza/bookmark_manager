9184ac889014eebd5a83a74bd205430481528708
Tabish Raza <tabishraza@gmail.com>
2017-11-17 16:25:37 +0000

    sign in test

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,6 +1,23 @@
-feature 'user can sign in' do
+feature 'User sign in' do
+let!(:user) do
+  User.create(
+    email: 'user@example.com',
+    password: '1234',
+    password_confirmation: '1234'
+  )
+  end
+
   scenario 'directed to links page with welcome message upon sign in' do
-    sign_in('123@test.com', '12345')
-    expect(page).to have_content('Welcome 123@test.com')
+    sign_in(email: user.email, password: user.password)
+    expect(page).to have_content("Welcome #{user.email}")
+  end
+
+  def sign_in(email:, password:)
+    visit '/'
+    within('form#sign_in') do
+      fill_in 'email', with: email
+      fill_in 'password', with: password
+      click_button 'Sign in'
+    end
   end
 end
