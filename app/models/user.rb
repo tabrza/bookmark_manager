4b47e2192079f1f4181c63699466e4eb7f53889a
Tabish Raza <tabishraza@gmail.com>
2017-11-17 16:24:48 +0000

    authenticate method

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,16 @@ class User
     self.encrypted_password = BCrypt::Password.create(password)
   end
 
+  def self.authenticate(email, password)
+    user = first(email: email)
+    if user && BCrypt::Password.new(user.encrypted_password) == password
+      # return this user
+      user
+    else
+      nil
+    end
+  end
+
   validates_confirmation_of :password
   validates_presence_of :email
   validates_format_of :email, as: :email_address
